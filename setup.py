7b3033eb017628c6389babb41d3bf73faf958b3c
Dipesh Chauhan <dipesh@chauhan.net.au>
2020-07-22 22:13:28 -0700

    Added move option
    * Add a move instead of copy option triggered by the file name having "move" in it.
    * Moved to setup.py for tracking dependencies instead of requirement.txt.
    * Added autopep8 and moved flake8 setting to .flake8 file.
    * Removed ubuntu builds as we never test it or use it.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="photo-organizer",
+    description="A quick and dirty script/application designed to allow my wife to quickly pre-process 10s of gigabytes of photos she found on some old hard drives. These drives had different organization patterns and many drives contained duplicates.",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        "altgraph==0.17",
+        "ImageHash==4.1.0",
+        "macholib==1.14",
+        "numpy==1.18.4",
+        "Pillow==7.1.2",
+        "PyInstaller==3.6",
+        "PyWavelets==1.1.1",
+        "scipy==1.4.1",
+        "six==1.15.0"
+    ],
+    extras_require={
+        "dev": [
+            "autopep8==1.5.3",
+            "flake8==3.8.1"
+        ]
+    }
+)
